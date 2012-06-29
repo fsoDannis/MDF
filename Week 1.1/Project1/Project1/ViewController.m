9fe404ea83b53f0ad684b0d13aef9ba6158b81c8
Dan <Dan@Dinkydetails.com>
2012-06-29 14:32:37 -0400

    Possibly fixed issue

---
+++ Week 1.1/Project1/Project1/ViewController.m
@@ -0,0 +1,156 @@
+// MDF Project 1 _ Week 1
+//  Created by Dan Annis on 6/28/12.
+//  Copyright (c) 2012. All rights reserved.
+
+
+#import "ViewController.h"
+#import "CustomTableCell.h"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+
+
+
+- (void)viewDidLoad
+{
+    
+    detailScreen.frame = CGRectMake(-320.0f, 0.0f, detailScreen.frame.size.width, detailScreen.frame.size.height);
+    infoScreen.frame = CGRectMake(0.0f, -480.0f, infoScreen.frame.size.width, infoScreen.frame.size.height);
+    
+    myArray = [[NSMutableArray alloc] initWithObjects:
+                 @"Dan Annis",
+                 @"Emily Annis",
+                 @"Alex Annis",
+                 @"Makenzie Annis",
+                 @"Chris Annis",
+                 @"Sarah Annis",
+                 @"Natalie Annis",
+                 @"Kate Annis",
+                 @"Chad Cunningham",
+                 @"Angela Frisch",
+                 @"Bobby Frisch",
+                 @"Isaac Frisch",
+                 @"Saif Iqbal",
+                 @"Anthony Annis",
+                 @"Andrew Annis",
+                 @"Stephanie Mascari",
+                 @"Lynn Annis",
+                 @"Ken Annis",
+                 @"Tina Mascari",
+                 @"Jeff Mascari",
+                 nil];
+    
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+ 
+    tableView = nil;
+    
+
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+
+- (IBAction)Edit:(id)sender
+{
+    if (tableView.editing == NO)
+    {
+        [tableView setEditing:YES];
+    }
+    else
+        [tableView setEditing:NO];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [myArray count];
+}
+
+
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return UITableViewCellEditingStyleDelete;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete)
+    {
+        NSLog(@"Deleted row - %@", [myArray objectAtIndex:indexPath.row]);
+        [myArray removeObjectAtIndex:indexPath.row];
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+    }
+}
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil)
+    {
+        cell =[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+
+        
+    }
+    cell.textLabel.text=(NSString*)[myArray objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+
+    //NSLog(@"Index Row = %d Index name= %@",indexPath.row, [myArray objectAtIndex:indexPath.row]);
+    
+    NSString *imageLink = @".jpg";
+    NSString *imageTag =  [NSString stringWithFormat:@"%@%@",[myArray objectAtIndex:indexPath.row],imageLink];
+    detailName.text = [myArray objectAtIndex:indexPath.row];
+    detailImage.image = [UIImage imageNamed:imageTag];
+    
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:1.0];
+    detailScreen.frame = CGRectMake(0.0f, 0.0f, detailScreen.frame.size.width, detailScreen.frame.size.height);
+    
+    [UIView commitAnimations];
+
+}
+
+
+- (IBAction)Done:(id)sender
+{
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:1.0];
+
+    detailScreen.frame = CGRectMake(-320.0f, 0.0f, detailScreen.frame.size.width, detailScreen.frame.size.height);
+    infoScreen.frame = CGRectMake(0.0f, -480.0f, infoScreen.frame.size.width, infoScreen.frame.size.height);
+    [UIView commitAnimations];
+}
+
+- (IBAction)info:(id)sender
+{
+    [UIView beginAnimations:nil context:nil];
+
+    infoScreen.frame = CGRectMake(0.0f, 0.0f, infoScreen.frame.size.width, infoScreen.frame.size.height);
+    
+    [UIView commitAnimations];
+}
+
+@end
--- Week 1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/CustomTableCell.h
+++
@@ -1,9 +0,0 @@
-// MDF Project 1 _ Week 1
-//  Created by Dan Annis on 6/28/12.
-//  Copyright (c) 2012. All rights reserved.
-
-#import <UIKit/UIKit.h>
-
-@interface CustomTableCell : UITableViewCell
-
-@end
