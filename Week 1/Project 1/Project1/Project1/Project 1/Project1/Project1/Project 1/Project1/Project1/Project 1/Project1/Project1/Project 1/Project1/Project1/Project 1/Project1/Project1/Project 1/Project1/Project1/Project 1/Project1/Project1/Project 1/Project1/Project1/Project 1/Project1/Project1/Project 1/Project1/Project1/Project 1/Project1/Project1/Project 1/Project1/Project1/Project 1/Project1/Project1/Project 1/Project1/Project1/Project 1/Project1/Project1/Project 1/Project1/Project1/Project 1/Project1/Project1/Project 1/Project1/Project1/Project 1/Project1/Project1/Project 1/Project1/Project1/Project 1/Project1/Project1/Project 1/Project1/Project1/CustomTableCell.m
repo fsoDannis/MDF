0f64ca0fb786b04145da56de153625457f7589b4
Dan <Dan@Dinkydetails.com>
2012-06-28 23:28:31 -0400

    Fixing images

---
+++ Week 1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/CustomTableCell.m
@@ -0,0 +1,28 @@
+// MDF Project 1 _ Week 1
+//  Created by Dan Annis on 6/28/12.
+//  Copyright (c) 2012. All rights reserved.
+
+
+
+#import "ViewController.h"
+#import "CustomTableCell.h"
+
+@implementation CustomTableCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ Week 1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/Project 1/Project1/Project1/CustomTableCell.h
@@ -0,0 +1,9 @@
+// MDF Project 1 _ Week 1
+//  Created by Dan Annis on 6/28/12.
+//  Copyright (c) 2012. All rights reserved.
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableCell : UITableViewCell
+
+@end
