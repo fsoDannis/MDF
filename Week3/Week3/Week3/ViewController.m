d8812b3655736c3ffa0a5475e77bb56041bbe1b7
Dan <Dan@Dinkydetails.com>
2012-07-25 07:05:59 -0400

    another change

--- Week3/Week3/Week3/ViewController.m
+++ Week3/Week3/Week3/ViewController.m
@@ -104,7 +104,7 @@
             NSString *fullLocation =  [NSString stringWithFormat:@"%@, %@",city,state];
             destViewController.stateName = fullLocation;
             destViewController.title = [[tableData objectAtIndex:indexPath.row] objectForKey:@"fullName"];
-            destViewController.coord = CLLocationCoordinate2DMake([lat floatValue],[lon floatValue]);
+           
             // Hide bottom tab bar in the detail view
            destViewController.hidesBottomBarWhenPushed = YES;
             
@@ -119,6 +119,7 @@
             NSString *fullLatLon =  [NSString stringWithFormat:@"%@, %@",lat,lon];
             destViewController.stateName = fullLocation;
             destViewController.latlonName = fullLatLon;
+            destViewController.coord = CLLocationCoordinate2DMake([lat floatValue],[lon floatValue]);
             destViewController.title = [[tableData objectAtIndex:indexPath.row] objectForKey:@"fullName"];
             
             // Hide bottom tab bar in the detail view
